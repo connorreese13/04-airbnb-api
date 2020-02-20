c0394e180b73b6a3896810273e385cb8f32d80a6
Connor Reese <connor.reese13@gmail.com>
2020-02-20 12:56:15 +0800

    started server and db connected

--- database.js
+++ database.js
@@ -1,9 +1,13 @@
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true }, err => {
-	if (err) {
-		console.log(err)
-	} else {
-		console.log('Connected to MongoDB')
-	}
-})
-module.exports = mongoose
+const mongoose = require("mongoose");
+mongoose.connect(
+  process.env.MONGODB_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  err => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connected to MongoDB");
+    }
+  }
+);
+module.exports = mongoose;
