abded7e48473317be6cad80872f1ae253949e5c7
Connor Reese <connor.reese13@gmail.com>
2020-02-20 15:49:08 +0800

    added shit

--- controllers/amenities.js
+++ controllers/amenities.js
@@ -2,20 +2,9 @@ const Amenities = require("../models/amenities");
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  console.log("hello");
-  Amenities.find({})
-    .populate({
-      path: "amenities",
-      select: "name"
-    })
-    .then(amenities => {
-      console.log(amenities);
-      // Set first of images as 'image'
-      res.send(amenities);
-    })
-    .catch(err => {
-      res.send(err);
-    });
+  Amenities.find().then(response => {
+    res.send(response);
+  });
 });
 
 module.exports = router;
